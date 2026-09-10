4c085d821814ef8f825058c1db06919aa50efc82
gagomaho1888 <gagomaho18@gmail.com>
2026-09-10 18:19:53 +0000

    Ajout du domaine ghalisam.onrender.com

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = _secret
 
 DEBUG = env_bool('DJANGO_DEBUG', False)
 
-ALLOWED_HOSTS = env_list('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ghalisam.onrender.com']
 
 # Production security – active seulement quand DEBUG=False
 if not DEBUG:
